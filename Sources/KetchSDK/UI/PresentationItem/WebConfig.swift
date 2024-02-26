674e05a1e4887961c47061623aa906bb4feca7e9
Roman Simenok <roma.simenok@gmail.com>
2024-02-26 14:34:03 +0200

    added back mobile_device parameter (#32)

--- Sources/KetchSDK/UI/PresentationItem/WebConfig.swift
+++ Sources/KetchSDK/UI/PresentationItem/WebConfig.swift
@@ -57,7 +57,8 @@ struct WebConfig {
     private var queryItems: [URLQueryItem] {
         var defaultQuery = [
             "propertyName": URLQueryItem(name: "propertyName", value: propertyName),
-            "orgCode":      URLQueryItem(name: "orgCode", value: orgCode),
+            "orgCode": URLQueryItem(name: "orgCode", value: orgCode),
+            "mobile_device": URLQueryItem(name: "mobile_device", value: UIDevice.current.userInterfaceIdiom == .phone ? "phone" : "tablet")
         ]
         
         params.forEach {

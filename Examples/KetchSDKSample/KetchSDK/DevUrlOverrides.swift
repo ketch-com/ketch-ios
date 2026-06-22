e4a081821c0dab6b066da3c2eee5f885a3030c43
ethanpschoen <ethan.schoen@ketch.com>
2026-06-22 13:31:33 -0700

    fix default

--- Examples/KetchSDKSample/KetchSDK/DevUrlOverrides.swift
+++ Examples/KetchSDKSample/KetchSDK/DevUrlOverrides.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Flip `enabled` to redirect UAT tag script URLs to local dev servers.
 enum DevUrlOverrides {
-    static let enabled = true
+    static let enabled = false
 
     private static let localKetchSdk: [String: String] = [
         "https://cdn.uat.ketchjs.com/ketchtag/stable/v2.12/ketch-sdk.js": "http://localhost:9000/ketch-sdk.js",
